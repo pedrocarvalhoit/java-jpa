9352547e8a9cb8033559ae3e57472b7a54c2aca1
Pedro Duarte <pedrohdc@yahoo.com.br>
2022-09-14 11:10:47 +0100

    jpa gym

--- src/main/java/br/com/academia/teste/CadastroCliente.java
+++ src/main/java/br/com/academia/teste/CadastroCliente.java
@@ -14,48 +14,41 @@ import br.com.academia.modelo.ProgramaTreino;
 import br.com.academia.util.JpaUtil;
 
 public class CadastroCliente {
-/** Adicionar numero de sócio */
-/** Criar métodos busca*/
+	/** Adicionar numero de sócio */
+	/** Criar métodos busca */
 	public static void main(String[] args) {
-		//Cadastrando modalidades programas e clientes
+		// Cadastrando modalidades programas e clientes
 		Modalidade musculacao = new Modalidade("Musculação", "Ginásio");
 		Modalidade corridaIndoor = new Modalidade("Corrida Indoor", "Passadeira");
-		
+
 		ProgramaTreino musculacaoIntermediario1 = new ProgramaTreino("Treino Hipertrofia 2", 60, musculacao);
 		ProgramaTreino corridaIndoorIniciate2 = new ProgramaTreino("Corrida Indoor 2", 60, corridaIndoor);
-		
+
 		Cliente pedro = new Cliente(000001, "Pedro Duarte", "Intermediário", musculacao, musculacaoIntermediario1);
-		Cliente thamires = new Cliente(000002, "Thamires Guimaraes", "Iniciante", corridaIndoor, corridaIndoorIniciate2);
-		
-		
+		Cliente thamires = new Cliente(000002, "Thamires Guimaraes", "Iniciante", corridaIndoor,	corridaIndoorIniciate2);
+
 		EntityManager em = JpaUtil.getEntityManager();
-		
+
 		ProgramaTreinoDao programaDAO = new ProgramaTreinoDao(em);
 		ModalidadeDao modalidadeDAO = new ModalidadeDao(em);
 		ClienteDao clienteDAO = new ClienteDao(em);
-		
+
 		em.getTransaction().begin();
-		
+
 		programaDAO.cadastrar(musculacaoIntermediario1);
-		programaDAO.cadastrar(corridaIndoorIniciate2);		
-		
+		programaDAO.cadastrar(corridaIndoorIniciate2);
+
 		modalidadeDAO.cadastrar(musculacao);
 		modalidadeDAO.cadastrar(corridaIndoor);
-		
+
 		clienteDAO.cadastrar(pedro);
 		clienteDAO.cadastrar(thamires);
-		
+
 		em.getTransaction().commit();
 		em.close();
-		System.out.println(pedro.getNivelDeAtividade());
-	
-		EntityManager em2 = JpaUtil.getEntityManager();
-		ListaClientesDao lcDAO = new ListaClientesDao(em2);
 		
-		List<Cliente> listaClientes = lcDAO.listaClientes();
-		listaClientes.stream()
-			.forEach(c -> System.out.println(c.getPrograma() ));
-			
+
+	
 	}
 
 }
---
+++ src/main/java/br/com/academia/teste/ListaClientes.java
@@ -0,0 +1,51 @@
+package br.com.academia.teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.academia.dao.ClienteDao;
+import br.com.academia.dao.ListaClientesDao;
+import br.com.academia.dao.ModalidadeDao;
+import br.com.academia.dao.ProgramaTreinoDao;
+import br.com.academia.modelo.Cliente;
+import br.com.academia.modelo.Modalidade;
+import br.com.academia.modelo.ProgramaTreino;
+import br.com.academia.util.JpaUtil;
+
+public class ListaClientes {
+	
+	public static void main(String[] args) {
+		
+		Modalidade musculacao = new Modalidade("Musculação", "Ginásio");
+		Modalidade corridaIndoor = new Modalidade("Corrida Indoor", "Passadeira");
+		ProgramaTreino musculacaoIntermediario1 = new ProgramaTreino("Treino Hipertrofia 2", 60, musculacao);
+		ProgramaTreino musculacaoAvançado3 = new ProgramaTreino("Corrida Indoor 2", 60, corridaIndoor);
+		Cliente pedro = new Cliente(000003, "Pedro Duarte", "Intermediário", musculacao, musculacaoIntermediario1);
+		Cliente victor = new Cliente(000004, "Victor Duarte", "Avançado", corridaIndoor,	musculacaoAvançado3);
+		
+		EntityManager em = JpaUtil.getEntityManager();
+		ModalidadeDao md = new ModalidadeDao(em);
+		ProgramaTreinoDao ptd = new ProgramaTreinoDao(em);
+		ClienteDao cd = new ClienteDao(em);
+		
+		em.getTransaction().begin();
+		
+		md.cadastrar(musculacao);
+		md.cadastrar(corridaIndoor);
+		ptd.cadastrar(musculacaoIntermediario1);
+		ptd.cadastrar(musculacaoAvançado3);
+		cd.cadastrar(pedro);
+		cd.cadastrar(victor);
+		
+		em.getTransaction().commit();
+		
+		ListaClientesDao ld = new ListaClientesDao(em);
+		List<Cliente> listaClientes = ld.listaClientes();
+		listaClientes.stream().forEach(c -> System.out.println("Nome: " + c.getNome() + " CPF: " +  c.getCpf()));
+		em.close();
+		
+		
+	}
+
+}
